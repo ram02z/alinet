3247422f77d58d17c8b39cc21126578f6a999a95
zawar ali <ali.zawar.it@gmail.com>
2023-12-22 12:37:42 +0000

    Refactored video_processing code - cleaned up code - now video_processing returns only the top n distinct frames that correspond to the top n slides in the provided pdf

--- video_processing.py
+++ video_processing.py
@@ -1,97 +1,86 @@
 import cv2
 import numpy as np
+from fitz import fitz
 
 def is_frame_different(frame1, frame2, threshold=0.9):
-    # Convert frames to grayscale
     gray_frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
     gray_frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
     abs_diff = cv2.absdiff(gray_frame1, gray_frame2)
-    # Calculate the mean of absolute differences
     mean_abs_diff = np.mean(abs_diff)
     return mean_abs_diff > 1
 
-# Initialize list to store tuples (frame, start time, end time)
-different_frames_list = []
-video_path = 'sample_data/lecture.mp4'
-cap = cv2.VideoCapture(video_path)
+def convert_millis_to_time_format(millis):
+    seconds = int(millis / 1000)
+    minutes, seconds = divmod(seconds, 60)
+    return f"{minutes:02d}:{seconds:02d}"
 
-if not cap.isOpened():
-    print("Error: Could not open video file.")
-    exit()
 
-# Read the first frame to initialize previous_frame
-ret, previous_frame = cap.read()
+def main():
+    video_path = 'sample_data/lecture.mp4'
+    slides_path = 'sample_data/hai_lecture_slides.pdf'
 
-if not ret:
-    print("End of video.")
-    exit()
+    cap = cv2.VideoCapture(video_path)
 
-# Loop to read and process every n frames
-frame_interval = 300
-while True:
-    # Read the next frame
-    for _ in range(frame_interval - 1):
-        ret, _ = cap.read()  # Skip frames
+    if not cap.isOpened():
+        print("Error: Could not open video file.")
+        exit()
+
+    slide_num = 0
+    with fitz.open(slides_path, filetype="pdf") as doc:
+        slide_num = len(doc)
+
+    # Initialize list to store tuples (text, start time, end time)
+    slide_chunks = []
+
+    ret, previous_frame = cap.read()
+    frame_interval = int(cap.get(cv2.CAP_PROP_FPS) * 10)
+    i = 0
+
+    while i <= slide_num:
+        for _ in range(frame_interval - 1):
+            ret, _ = cap.read()  # Skip frames
+            if not ret:
+                print("End of video.")
+                break
+
+        ret, frame = cap.read()
         if not ret:
             print("End of video.")
             break
 
-    ret, frame = cap.read()
-    if not ret:
-        print("End of video.")
-        break
+        current_time_millis = cap.get(cv2.CAP_PROP_POS_MSEC)
+        timestamp = convert_millis_to_time_format(current_time_millis)
 
-    # Get the current position in milliseconds
-    current_time_millis = cap.get(cv2.CAP_PROP_POS_MSEC)
+        if is_frame_different(frame, previous_frame):
+            i += 1
+            cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            cv2.imshow('Original Frame', frame)
 
-    # Convert milliseconds to readable time format (assuming 30 frames per second)
-    current_time_sec = current_time_millis / 1000.0
-    minutes = int(current_time_sec / 60)
-    seconds = int(current_time_sec % 60)
-    timestamp = f"{minutes:02d}:{seconds:02d}"
+            # Add tuple (text, start time, end time) to the list
+            slide_chunks.append((None, timestamp, None))
 
-    if is_frame_different(frame, previous_frame):
-        cv2.putText(frame, timestamp, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow('Original Frame', frame)
+        previous_frame = frame.copy()
 
-        # Add tuple (frame, start time, end time) to the list
-        different_frames_list.append((frame.copy(), timestamp, None))
+        if cv2.waitKey(25) & 0xFF == ord('q'):
+            break
 
-    # Set the current frame as the previous frame for the next iteration
-    previous_frame = frame.copy()
+    cap.release()
+    cv2.destroyAllWindows()
 
-    # Break the loop if the user presses 'q'
-    if cv2.waitKey(25) & 0xFF == ord('q'):
-        break
+    # Set each tuple's text to be the one from the slides
+    with fitz.open(slides_path, filetype="pdf") as doc:
+        for i, page in enumerate(doc):
+            text = page.get_text()
+            slide_chunks[i] = (text, slide_chunks[i][1], slide_chunks[i][2])
 
-# Update end time for the last frame in the list
-if different_frames_list:
-    # Calculate the duration of the video using the frame rate and total frames
-    frame_rate = cap.get(cv2.CAP_PROP_FPS)
-    total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-    duration_sec = total_frames / frame_rate
-    # Convert seconds to minutes and seconds
-    minutes = int(duration_sec // 60)
-    seconds = int(duration_sec % 60)
+    # Set each tuple's end time to be the next tuple's start time
+    for i in range(len(slide_chunks)-1):
+        next_frame_tuple = slide_chunks[i + 1]
+        slide_chunks[i] = (slide_chunks[i][0], slide_chunks[i][1], next_frame_tuple[1])
 
-    # Format the result as "minutes:seconds"
-    formatted_time = f"{minutes}:{seconds:02d}"
+    # Remove lase element of frame_list
+    slide_chunks.pop()
 
-    # Set the end time of the last frame to the video duration
-    different_frames_list[-1] = (different_frames_list[-1][0], different_frames_list[-1][1], formatted_time)
+    return slide_chunks
 
-# Release the video capture object and close windows
-cap.release()
-cv2.destroyAllWindows()
 
-# Print the list of different frames and their timestamps
-print("Different Frames List:")
-for i in range(len(different_frames_list)-1):
-    frame_tuple = different_frames_list[i]
-    next_frame_tuple = different_frames_list[i + 1]
-    frame_tuple = (frame_tuple[0], frame_tuple[1], next_frame_tuple[1])
-    print(f"Start Time: {frame_tuple[1]}, End Time: {frame_tuple[2]}")
-
-# Print the last frame separately (no next frame)
-last_frame_tuple = different_frames_list[-1]
-print(f"Start Time: {last_frame_tuple[1]}, End Time: {last_frame_tuple[2]}")
