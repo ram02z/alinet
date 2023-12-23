6f1a8bd4b35cdcc9b5386ec2cbaf5b5122edc7c6
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-12-23 16:54:32 +0000

    remove irrelevant finetuning files and update generate_dataset.py so that it outputs a combined dataset.csv file

--- finetune/generate_dataset.py
+++ finetune/generate_dataset.py
@@ -1,42 +1,91 @@
-from datasets import load_dataset
+from datasets import load_dataset, concatenate_datasets
 import pandas as pd
-from sklearn.utils import shuffle
+from sklearn.model_selection import train_test_split
 import os
 
-def preprocess_dataset(dataset):
-  df_dataset = pd.DataFrame(columns=['context', 'question'])
 
-  for idx in range(0, len(dataset['id'])):
-      context = dataset['context'][idx]
-      question = dataset['question'][idx]
+dataset_save_path = 'datasets/'
 
-      df_dataset.loc[idx] = [context] + [question] 
+def load_squad_dataset():
+  df_dataset = pd.DataFrame(columns=['dataset', 'context', 'question'])
+  dataset = load_dataset("squad")  
+
+  # combined = train and validation which includes test
+  combined_dataset = concatenate_datasets([dataset["train"], dataset["validation"]])
+
+  for idx in range(0, len(combined_dataset['question'])):
+      context = combined_dataset['context'][idx]
+      question = combined_dataset['question'][idx]
+
+      data = {'dataset': 'squad', 'context': context, 'question': question}
+      df_dataset.loc[idx] = data
     
   return df_dataset
 
+def load_race_dataset():
+  df_dataset = pd.DataFrame(columns=['dataset', 'context', 'question'])
+  dataset = load_dataset("race", "all")  
+
+  # combined = train and validation which includes test
+  combined_dataset = concatenate_datasets([dataset["train"], dataset["validation"], dataset["test"]])
+  
+  for idx in range(0, len(combined_dataset['question'])):
+      context = combined_dataset['article'][idx]
+      question = combined_dataset['question'][idx]
+
+      # skip questions that does not contain ?, if it takes too long can do question[:-1]
+      if '?' not in question:
+         continue
+
+      data = {'dataset': 'race', 'context': context, 'question': question}
+      df_dataset.loc[idx] = data
+  
+  return df_dataset
+
+def load_sciq_dataset():
+  df_dataset = pd.DataFrame(columns=['dataset', 'context', 'question'])
+  dataset = load_dataset("sciq")  
+
+  # combined = train and validation which includes test
+  combined_dataset = concatenate_datasets([dataset["train"], dataset["validation"], dataset["test"]])
+
+  for idx in range(0, len(combined_dataset['question'])):
+      context = combined_dataset['support'][idx]
+      question = combined_dataset['question'][idx]
+
+      data = {'dataset': 'sciq', 'context': context, 'question': question}
+      df_dataset.loc[idx] = data
+  
+  return df_dataset
+
+def load_yahoo_dataset():
+  df_dataset = pd.DataFrame(columns=['dataset', 'context', 'question'])
+  # only contain train split
+  dataset = load_dataset("yahoo_answers_qa", split="train")
+
+  for idx in range(0, len(dataset['question'])):
+      context = dataset['answer'][idx]
+      question = dataset['question'][idx]
+
+      data = {'dataset': 'yahoo', 'context': context, 'question': question}
+      df_dataset.loc[idx] = data
+  
+  return df_dataset
+
 if __name__ == "__main__":
-  train_dataset = load_dataset("squad", split='train')
-  validate_dataset = load_dataset("squad", split='validation')
+  df_list = []
 
+  df_list.append(load_squad_dataset())
+  df_list.append(load_race_dataset())
+  df_list.append(load_sciq_dataset())
+  df_list.append(load_yahoo_dataset())
 
-  # print(validate_dataset)
-
-  df_train = preprocess_dataset(train_dataset)
-  df_validate = preprocess_dataset(validate_dataset)
-
-  # shuffling helps with training process, not sure if necessary
-  df_train = shuffle(df_train)
-  df_validate = shuffle(df_validate)
-
-  #print('train: ', len(df_train))
-  #print('validation: ', len(df_validate))
+  # Concatenate the all datasets
+  combined_df = pd.concat(df_list, ignore_index=True)
 
   dataset_save_path = 'datasets/'
   if not os.path.exists(dataset_save_path):
-      os.makedirs(dataset_save_path)
-  df_train.to_csv(dataset_save_path + 'squad_train.csv', index=False)
-  df_validate.to_csv(dataset_save_path + 'squad_validation.csv', index=False)
+    os.makedirs(dataset_save_path)
+  combined_df.to_csv(dataset_save_path + 'dataset.csv', index=False)
 
-# task, generate sum dataset in csv file
-# split dataset in train val test split 80, 10, 10
-# preprocess so that it only contains ? 
+
