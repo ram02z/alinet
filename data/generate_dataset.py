716607da67e540f78857955dd2d1419782dd9494
Jia Wei Hong <jiaweihong1@hotmail.com>
2023-12-25 21:55:09 +0000

    feat: add dataset generation script (#14)
    * Add files that generate squad dataset csv and finetune a t5-base model
    
    * remove irrelevant finetuning files and update generate_dataset.py so that it outputs a combined dataset.csv file
    
    * remove irrelevant datasets, filter questions marks and unique contexts
    
    * Add other datasets
    
    * Bump datasets package
    
    ---------
    
    Co-authored-by: Omar Zeghouani <omarzeghouanii@gmail.com>

---
+++ data/generate_dataset.py
@@ -0,0 +1,113 @@
+import logging
+import os
+from dataclasses import dataclass, field
+
+from datasets import load_dataset, concatenate_datasets
+from transformers import set_seed, HfArgumentParser
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class GenerateDatasetArguments:
+    seed: int = field(default=42, metadata={"help": "Random seed"})
+    remove_duplicate_context: bool = field(
+        default=True,
+        metadata={"help": "Keep only one context for each question"},
+    )
+
+
+def contain_question_mark(data):
+    return data["target"][-1].rstrip() == "?"
+
+
+def contain_unique_question_context(data, unique_sources):
+    if data["source"] not in unique_sources:
+        unique_sources.add(data["source"])
+        return True
+    return False
+
+
+def normalise(data):
+    # Lowercase the text
+    data["source"] = data["source"].lower()
+    data["target"] = data["target"].lower()
+
+    # Remove new line characters
+    data["source"] = data["source"].replace("\n", " ")
+
+    return data
+
+
+def main():
+    parser = HfArgumentParser((GenerateDatasetArguments,))
+    args = parser.parse_args_into_dataclasses()[0]
+
+    set_seed(args.seed)
+
+    logger.info("loading datasets")
+
+    squad_data = (
+        load_dataset("squad", split="train+validation")
+        .select_columns(["context", "question"])
+        .rename_columns({"context": "source", "question": "target"})
+    )
+    adversarial_data = (
+        load_dataset("adversarial_qa", "adversarialQA", split="train+validation+test")
+        .select_columns(["context", "question"])
+        .rename_columns({"context": "source", "question": "target"})
+    )
+    narrative_data = (
+        load_dataset("narrativeqa", split="train+validation+test")
+        .select_columns(["document", "question"])
+        .map(
+            lambda x: {
+                "document": x["document"]["summary"]["text"],
+                "question": x["question"]["text"],
+            }
+        )
+        .rename_columns({"document": "source", "question": "target"})
+    )
+    fairytale_data = (
+        load_dataset("GEM/FairytaleQA", split="train+validation+test")
+        .filter(lambda x: x["ex_or_im"] == "explicit")
+        .select_columns(["content", "target"])
+        .rename_columns({"content": "source"})
+    )
+
+    logger.info("concatenating datasets")
+
+    dataset = concatenate_datasets(
+        [squad_data, adversarial_data, narrative_data, fairytale_data]
+    )
+
+    logger.info("filtering datasets")
+
+    dataset = dataset.filter(contain_question_mark)
+    if args.remove_duplicate_context:
+        unique_sources = set()
+        dataset = dataset.filter(
+            contain_unique_question_context,
+            fn_kwargs={"unique_sources": unique_sources},
+        )
+    dataset = dataset.map(normalise)
+
+    logger.info("splitting datasets")
+
+    train_testvalid = dataset.train_test_split(test_size=0.1, seed=42)
+    test_valid = train_testvalid["test"].train_test_split(test_size=0.5, seed=42)
+    train = train_testvalid["train"]
+    valid = test_valid["train"]
+    test = test_valid["test"]
+
+    logger.info("saving datasets")
+
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+
+    train.to_csv(os.path.join(script_dir, "train.csv"))
+    valid.to_csv(os.path.join(script_dir, "valid.csv"))
+    test.to_csv(os.path.join(script_dir, "test.csv"))
+
+
+if __name__ == "__main__":
+    main()
