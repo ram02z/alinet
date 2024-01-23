73981a57803860aa544ce1d1d20d23d0fecfbee5
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-01-23 15:09:57 +0000

    refactor: update dataset generation script

--- data/generate_dataset.py
+++ data/generate_dataset.py
@@ -4,18 +4,20 @@ from dataclasses import dataclass, field
 
 from datasets import load_dataset, concatenate_datasets
 from transformers import set_seed, HfArgumentParser
+from strenum import StrEnum
 
 logger = logging.getLogger(__name__)
 
 
+class Dataset(StrEnum):
+    BASELINE_TRAIN = "baseline_train"
+    RC_EVAL = "reading_comprehension_eval"
+
 @dataclass
 class GenerateDatasetArguments:
+    dataset: Dataset = field(metadata={"help": "Name of the dataset"})
     seed: int = field(default=42, metadata={"help": "Random seed"})
-    remove_duplicate_context: bool = field(
-        default=True,
-        metadata={"help": "Keep only one context for each question"},
-    )
-
+    data_dir: str = field(default="data", metadata={"help": "Output data directory"})
 
 def contain_question_mark(data):
     return data["target"][-1].rstrip() == "?"
@@ -46,58 +48,35 @@ def main():
     set_seed(args.seed)
 
     logger.info("loading datasets")
-
-    squad_data = (
-        load_dataset("squad", split="train+validation")
-        .select_columns(["context", "question"])
-        .rename_columns({"context": "source", "question": "target"})
-    )
-    adversarial_data = (
-        load_dataset("adversarial_qa", "adversarialQA", split="train+validation+test")
-        .select_columns(["context", "question"])
-        .rename_columns({"context": "source", "question": "target"})
-    )
-    narrative_data = (
-        load_dataset("narrativeqa", split="train+validation+test")
-        .select_columns(["document", "question"])
-        .map(
-            lambda x: {
-                "document": x["document"]["summary"]["text"],
-                "question": x["question"]["text"],
-            }
+    if args.dataset == Dataset.BASELINE_TRAIN:
+        data = (
+            load_dataset("squad")
+            .select_columns(["context", "question"])
+            .rename_columns({"context": "source", "question": "target"})
+            .filter(contain_question_mark)
+            .map(normalise)
         )
-        .rename_columns({"document": "source", "question": "target"})
-    )
-    fairytale_data = (
-        load_dataset("GEM/FairytaleQA", split="train+validation+test")
-        .filter(lambda x: x["ex_or_im"] == "explicit")
-        .select_columns(["content", "target"])
-        .rename_columns({"content": "source"})
-    )
-
-    logger.info("concatenating datasets")
-
-    dataset = concatenate_datasets(
-        [squad_data, adversarial_data, narrative_data, fairytale_data]
-    )
-
-    logger.info("filtering datasets")
-
-    dataset = dataset.filter(contain_question_mark)
-    if args.remove_duplicate_context:
-        unique_sources = set()
-        dataset = dataset.filter(
-            contain_unique_question_context,
-            fn_kwargs={"unique_sources": unique_sources},
+    elif args.dataset == Dataset.RC_EVAL:
+        # BUG: https://huggingface.co/datasets/mrqa/discussions/3
+        data = (
+            load_dataset("mrqa", split="test")
+            .select_columns(["context", "question"])
+            .rename_columns({"context": "source", "question": "target"})
+            .filter(contain_question_mark)
+            .map(normalise)
         )
-    dataset = dataset.map(normalise)
 
     logger.info("saving dataset")
 
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-
-    dataset.to_csv(os.path.join(script_dir, "dataset.csv"))
+    if args.dataset == Dataset.BASELINE_TRAIN:
+        data["train"].to_csv(os.path.join(args.data_dir, "train.csv"))
+        data["validation"].to_csv(os.path.join(args.data_dir, "validation.csv"))
+    elif args.dataset == Dataset.RC_EVAL:
+        data.to_csv(os.path.join(args.data_dir, "test.csv"))
 
 
 if __name__ == "__main__":
+    import datasets
+
+    datasets.logging.set_verbosity_debug()
     main()
