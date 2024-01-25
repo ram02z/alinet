5bee737f4552035bb4db644c343d002f3de4559c
Jia Wei Hong <jiaweihong1@hotmail.com>
2024-01-25 14:24:19 +0000

    merge generate_balanced_dataset.py into generate_dataset.py, generates a stratified train validation split of 80 20

--- data/generate_dataset.py
+++ data/generate_dataset.py
@@ -13,6 +13,7 @@ logger = logging.getLogger(__name__)
 class Dataset(StrEnum):
     BASELINE = "baseline"
     BASELINE_NOISE = "baseline_noise"
+    BASELINE_BALANCED = "baseline_balanced"
 
 @dataclass
 class GenerateDatasetArguments:
@@ -35,7 +36,56 @@ def normalise(data):
 
     return data
 
+def categorise_dataset(data):
+    if any(word in data['target'] for word in ["what"]):
+        data['category'] = "description"
+    elif any(word in data['target'] for word in ["where", "when", "who", "how many", "how much", "which", "how long"]):
+        data['category'] = "recall" 
+    elif any(word in data['target'] for word in ["how did", "how does", "how do", "compute", "calculate"]):
+        data['category'] = "method"
+    elif any(word in data['target'] for word in ["why"]):
+        data['category'] = "explanation"
+    elif any(word in data['target'] for word in ["compare", "difference"]):
+        data['category'] = "comparison" 
+    
+    return data
 
+def print_distribution(dataset):
+    method_ds = dataset.filter(lambda data: data["category"] == "method")
+    description_ds = dataset.filter(lambda data: data["category"] == "description")
+    explanation_ds = dataset.filter(lambda data: data["category"] == "explanation")
+    comparison_ds = dataset.filter(lambda data: data["category"] == "comparison")
+    recall_ds = dataset.filter(lambda data: data["category"] == "recall")
+
+    na_ds = dataset.filter(lambda data: data["category"] == "NA")
+
+    print("description distribution =" + str( len(description_ds) / len(dataset) * 100) + "%, count = " + str(len(description_ds)))
+    print("recall distribution = " + str( len(recall_ds) / len(dataset) * 100) + "%, count = " + str(len(recall_ds)))
+    print("explanation distribution = " + str( len(explanation_ds) / len(dataset) * 100) + "%, count = " + str(len(explanation_ds)))
+    print("method distribution = " + str( len(method_ds) / len(dataset) * 100) + "%, count = " + str(len(method_ds)))
+    print("comparison distribution = " + str( len(comparison_ds) / len(dataset) * 100) + "%, count = " + str(len(comparison_ds)))
+    print("na distribution = " + str( len(na_ds) / len(dataset) * 100) + "%, count = " + str(len(na_ds)))
+
+def remove_na_category(data):
+    if data['category'] == 'NA':
+        return False
+    else:
+        return True
+
+def remove_excess_desc_recall(data, countDict):
+    if data['category'] == "description" and countDict['desc_count'] < 15970:
+        countDict['desc_count'] += 1
+        return True
+    elif data['category'] == "description" and countDict['desc_count'] >= 15970:
+        return False
+    elif data['category'] == "recall" and countDict['recall_count'] < 15970:
+        countDict['recall_count'] += 1
+        return True
+    elif data['category'] == "recall" and countDict['recall_count'] >= 15970:
+        return False
+    else:
+        return True
+    
 def main():
     parser = HfArgumentParser((GenerateDatasetArguments,))
     args = parser.parse_args_into_dataclasses()[0]
@@ -73,6 +123,74 @@ def main():
             [squad_data["validation"], spoken_squad_data["validation"]]
         )
         data = DatasetDict({"train": train_data, "validation": valid_data})
+    elif args.dataset == Dataset.BASELINE_BALANCED:
+            squad_data = (
+                load_dataset("squad", split="train+validation", trust_remote_code=True)
+                .select_columns(["context", "question"])
+                .rename_columns({"context": "source", "question": "target"})
+            )
+
+            adversarial_data = (
+                load_dataset("adversarial_qa", "adversarialQA", split="train+validation+test", trust_remote_code=True)
+                .select_columns(["context", "question"])
+                .rename_columns({"context": "source", "question": "target"})
+            )
+            narrative_data = (
+                load_dataset("narrativeqa", split="train+validation+test", trust_remote_code=True)
+                .select_columns(["document", "question"])
+                .map(
+                    lambda x: {
+                        "document": x["document"]["summary"]["text"],
+                        "question": x["question"]["text"],
+                    }
+                )
+                .rename_columns({"document": "source", "question": "target"})
+            )
+            fairytale_data = (
+                load_dataset("GEM/FairytaleQA", split="train+validation+test", trust_remote_code=True)
+                .filter(lambda x: x["ex_or_im"] == "explicit")
+                .select_columns(["content", "target"])
+                .rename_columns({"content": "source"})
+            )
+
+            sciq_data = (
+                load_dataset("sciq", split="train+validation+test", trust_remote_code=True)
+                .select_columns(["support", "question"])
+                .rename_columns({"support": "source", "question": "target"})
+            )
+
+            dataset = concatenate_datasets(
+                [squad_data, adversarial_data, narrative_data, fairytale_data, sciq_data]
+            )
+
+            dataset = dataset.filter(contain_question_mark)
+
+            dataset = dataset.map(normalise)
+
+            dataset = dataset.add_column("category", ["NA"] * len(dataset))
+
+            dataset = dataset.map(categorise_dataset)
+
+            comparative_dataset = load_dataset("alinet/comparativeQA", split='train')
+
+            dataset = concatenate_datasets(
+                [dataset, comparative_dataset]
+            )
+
+            dataset = dataset.filter(remove_na_category)
+
+            countDict = {"desc_count": 0, "recall_count": 0}
+
+            dataset = dataset.filter(remove_excess_desc_recall, fn_kwargs={"countDict": countDict})
+
+            dataset = dataset.class_encode_column("category")
+
+            dataset = dataset.train_test_split(test_size=0.2, stratify_by_column="category")
+
+            data = DatasetDict({"train": dataset["train"], "validation": dataset["test"]})
+
+            
+
 
     logger.info("saving dataset")
 
