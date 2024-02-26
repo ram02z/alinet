b365411bc33ee30091b88fde016e2137d7b0909d
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-02-26 14:02:38 +0000

    fix: update model names

--- src/alinet/qg/model.py
+++ src/alinet/qg/model.py
@@ -4,6 +4,6 @@ from strenum import StrEnum
 class Model(StrEnum):
     BASELINE = "alinet/bart-base-squad-qg"
     BASELINE_NOISE = "alinet/bart-base-spoken-squad-qg"
-    BALANCED = "alinet/base-base-balanced-qg"
-    BALANCED_RESOLVED = "alinet/base-base-balanced-resolved-qg"
-    BALANCED_RA = "alinet/base-base-balanced-ra-qg"
+    BALANCED = "alinet/bart-base-balanced-qg"
+    BALANCED_RESOLVED = "alinet/bart-base-balanced-resolved-qg"
+    BALANCED_RA = "alinet/bart-base-balanced-ra-qg"
