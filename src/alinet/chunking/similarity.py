02638220c4e2bb62dd715bc962d91aa2f16c8c6f
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-03-15 15:57:17 +0000

    refactor: update similarity chunking methods

--- src/alinet/chunking/similarity.py
+++ src/alinet/chunking/similarity.py
@@ -2,6 +2,8 @@ import spacy
 from sklearn.metrics.pairwise import cosine_similarity
 import warnings
 
+from alinet.chunking.model import TimeChunk
+
 
 def compute_cosine_similarity_word_embeddings(text1, text2):
     """
@@ -26,55 +28,68 @@ def compute_cosine_similarity_word_embeddings(text1, text2):
     return cosine_sim
 
 
-def get_similarity_scores(duration, transcript_chunks, slide_chunks):
+def find_matching_slide_range(
+    chunk: TimeChunk, slide_chunks: list[TimeChunk]
+) -> tuple[int, int]:
+    """
+    Finds the range of slide indices corresponding to the given transcript chunk.
+    """
+
+    start_index = 0
+    end_index = 0
+    for i, slide in enumerate(slide_chunks):
+        if chunk.start_time >= slide.start_time and chunk.end_time <= slide.end_time:
+            start_index = i
+            end_index = i
+            break
+        elif chunk.start_time < slide.end_time:
+            start_index = i
+        elif chunk.end_time > slide.end_time:
+            end_index = i
+    return start_index, end_index + 1  # Adjusted to get the correct range
+
+
+def get_similarity_scores(
+    transcript_chunks: list[TimeChunk], slide_chunks: list[TimeChunk]
+) -> list[float]:
     """
     Get similarity scores between transcript chunks and corresponding slide chunks.
 
     Parameters:
-    - duration (float): The duration until which transcript chunks are considered.
     - transcript_chunks (list): List of transcript chunks.
     - slide_chunks (list): List of slide chunks.
 
     Returns:
     list: A list of similarity scores between transcript chunks and corresponding slide chunks.
     """
-    i = 0
+    duration = transcript_chunks[-1].end_time
     similarity_scores = []
+
     for chunk in transcript_chunks:
-        # ensure we only process transcript chunks that occur before the final slide
-        if chunk["timestamp"][0] < duration:
-            list_of_slide_indices = []
-            while i < len(slide_chunks):
-                list_of_slide_indices.append(i)
-                if chunk["timestamp"][1] <= slide_chunks[i][2]:
-                    transcript_chunk_text = chunk["text"]
+        if chunk.start_time < duration:
+            start_index, end_index = find_matching_slide_range(chunk, slide_chunks)
+            slide_text = " ".join(
+                slide_chunks[i].text for i in range(start_index, end_index)
+            )
+            similarity_scores.append(
+                compute_cosine_similarity_word_embeddings(chunk.text, slide_text)
+            )
 
-                    # aggregate all the text from different slides, if more than one, into a single var
-                    slide_text = ""
-                    for index in list_of_slide_indices:
-                        slide_text += slide_chunks[index][0]
-
-                    # compute sim between the text retrieved from slide and the corresponding slide's text
-                    cosine_sim = compute_cosine_similarity_word_embeddings(
-                        transcript_chunk_text, slide_text
-                    )
-
-                    similarity_scores.append(cosine_sim)
-
-                    break
-                i += 1
     return similarity_scores
 
 
 def filter_questions_by_retention_rate(
-    sim_scores, generated_questions, similarity_threshold, filtering_threshold
-):
+    sim_scores: list[float],
+    generated_questions: list[str],
+    similarity_threshold: float,
+    filtering_threshold: float,
+) -> dict[int, str]:
     """
     Filter questions based on the retention rate and similarity threshold.
 
     Parameters:
     - sim_scores (list): List of similarity scores.
-    - generated_questions (dict): Dictionary of generated questions with respective indexes.
+    - generated_questions (list): List of generated questions.
     - similarity_threshold (float): Threshold for similarity scores.
     - filtering_threshold (float): Threshold for the retention rate.
 
@@ -83,8 +98,8 @@ def filter_questions_by_retention_rate(
     """
     filtered_questions = {}
 
-    for index, question in generated_questions.items():
-        if sim_scores[index] > similarity_threshold:
+    for index, (sim, question) in enumerate(zip(sim_scores, generated_questions)):
+        if sim > similarity_threshold:
             filtered_questions[index] = question
 
     retention_rate = len(filtered_questions) / len(generated_questions)
@@ -93,6 +108,6 @@ def filter_questions_by_retention_rate(
         warnings.warn(
             "Could not effectively perform question filtering, all generated questions are being returned"
         )
-        return generated_questions
+        return {idx: question for idx, question in enumerate(generated_questions)}
     else:
         return filtered_questions
--- src/alinet/main.py
+++ src/alinet/main.py
@@ -3,10 +3,8 @@ from alinet.chunking.similarity import (
     get_similarity_scores,
     filter_questions_by_retention_rate,
 )
-from alinet.chunking.video import (
-    slide_chunking, 
-    save_video_clips
-)
+from alinet.chunking.video import slide_chunking, save_video_clips
+
 
 def baseline(
     video_path: str,
@@ -14,8 +12,8 @@ def baseline(
     filtering_threshold,
     asr_model,
     qg_model,
-    video_clips_path
-) -> list[str]:
+    video_clips_path,
+) -> dict[int, str]:
     asr_pipe = asr.Pipeline(asr_model)
     whisper_chunks, duration = asr_pipe(video_path, batch_size=1)
     chunk_pipe = chunking.Pipeline(qg_model)
@@ -24,12 +22,12 @@ def baseline(
     if video_clips_path:
         save_video_clips(video_path, transcript_chunks, video_clips_path)
 
-    text_chunks = [chunk["text"] for chunk in transcript_chunks]
+    text_chunks = [chunk.text for chunk in transcript_chunks]
     qg_pipe = qg.Pipeline(qg_model)
     generated_questions = qg_pipe(text_chunks)
 
     slide_chunks = slide_chunking(video_path)
-    sim_scores = get_similarity_scores(duration, transcript_chunks, slide_chunks)
+    sim_scores = get_similarity_scores(transcript_chunks, slide_chunks)
     filtered_questions = filter_questions_by_retention_rate(
         sim_scores, generated_questions, similarity_threshold, filtering_threshold
     )
