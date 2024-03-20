ff3b45fa0ebde763231459fe1930e42951e51110
Omar Zeghouani <omarzeghouanii@gmail.com>
2024-03-20 16:45:10 +0000

    fix: make cors more general

--- server/main.py
+++ server/main.py
@@ -14,12 +14,15 @@ from alinet import asr, qg
 app = FastAPI()
 
 origins = [
-    "http://localhost:5173",
+    "*",
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
