bdb000b92015f4b27ed1547ed7668143eb2e1242
Omar Zeghouani <59267627+ram02z@users.noreply.github.com>
2024-03-26 21:10:44 +0000

    chore: add missing dep (#62)

--- web_app/src/components/PDFViewer.tsx
+++ web_app/src/components/PDFViewer.tsx
@@ -3,8 +3,10 @@ import { pdfjs, Document, Page } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import type { PDFDocumentProxy } from "pdfjs-dist";
-import { ActionIcon, Box, Group, Loader, Text } from "@mantine/core";
+import { ActionIcon, Box, Group, Loader, Space, Text } from "@mantine/core";
 import { IconChevronLeft, IconChevronRight } from "@tabler/icons-react";
+
+import classes from "./PDFViewer.module.css";
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.js",
@@ -17,6 +19,8 @@ const options = {
 };
 
 type PDFFile = string | File | null;
+
+const PDF_WIDTH = 700;
 
 export default function PDFViewer({ file }: { file: PDFFile }) {
   const [numPages, setNumPages] = useState<number>();
@@ -44,17 +48,8 @@ export default function PDFViewer({ file }: { file: PDFFile }) {
   }
 
   return (
-    <Box>
-      <Document
-        file={file}
-        loading={<Loader />}
-        onLoadStart={() => setLoading(true)}
-        onLoadSuccess={onDocumentLoadSuccess}
-        options={options}
-      >
-        <Page loading={<Loader />} pageNumber={pageNumber} width={600} />
-      </Document>
-      <Group mt="md" justify="center" w={600}>
+    <Box className={classes.Box}>
+      <Group justify="center">
         <ActionIcon
           loading={loading}
           onClick={previousPage}
@@ -73,6 +68,16 @@ export default function PDFViewer({ file }: { file: PDFFile }) {
           <IconChevronRight />
         </ActionIcon>
       </Group>
+      <Space h="md" />
+      <Document
+        file={file}
+        loading={<Loader />}
+        onLoadStart={() => setLoading(true)}
+        onLoadSuccess={onDocumentLoadSuccess}
+        options={options}
+      >
+        <Page loading={<Loader />} pageNumber={pageNumber} width={PDF_WIDTH} />
+      </Document>
     </Box>
   );
 }
